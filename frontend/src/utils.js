b04119dfd750223b48784b45ed9352dfd930fbcb
Surajit Manna <surajitmanna4551@gmail.com>
2026-05-03 10:23:52 +0530

    Added backend url

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,2 +1,1 @@
-// export const BACKEND_URL = 'https://blognest-vjoz.onrender.com'
-export const BACKEND_URL = 'http://localhost:5000'
+export const BACKEND_URL = 'https://blognest-xx4k.onrender.com'
